2da49d634703ff3aff35c0030bebc9cfa5a23159
JustCarmen <carmen@justcarmen.nl>
2014-08-24 12:35:01 +0200

    Fire functions only when needed.

--- theme-1.5.4/js/justlight.js
+++ theme-1.5.4/js/justlight.js
@@ -19,19 +19,13 @@
  */
 // use waitUntilExists plugin on pages with dynamic content - https://gist.github.com/md55/6565078
 
+/* General functions (fired on every page */
+
 // responsive page
 var $responsive = jQuery("#responsive").is(":visible");
 jQuery(window).resize(function() {
 	$responsive = jQuery("#responsive").is(":visible");
 });
-
-// Sticky footer - correction needed for pedigree page
-if(jQuery("#pedigree-page").length > 0) {
-	jQuery("#content").css("margin-bottom", "50px");
-}
-
-// Move link to change blocks to the footer area.
-jQuery("#link_change_blocks").appendTo(jQuery("#footer .top"));
 
 // Modal dialog boxes
 function jl_modalDialog(url, title) {
@@ -161,161 +155,6 @@ jQuery(document).ajaxComplete(function() {
 	jl_dialogBox();
 });
 
-// Styling of the individual page
-if(WT_SCRIPT_NAME === "individual.php") {
-	// Remove personboxNN class from header layout
-	jQuery("#indi_header H3").removeClass("person_boxNN");
-	
-	// relatives tab
-	jQuery("#relatives_content").waitUntilExists(function() {
-		jQuery(this).find(".subheaders").parents("table").css("margin-top", "15px");
-	});
-}
-
-// When in responsive state hide the indi_left part when sidebar is open.
-var responsiveSidebar = false;
-
-// Hide sidebar by default on smaller screens
-if ($responsive) {
-	jQuery.cookie("hide-sb", true);
-	responsiveSidebar = true;
-}
-
-jQuery(window).resize(function() {
-	jQuery("#indi_left").show();
-	if($responsive){
-		responsiveSidebar = true;
-	} else {
-		responsiveSidebar = false;
-	}
-	
-	if ($responsive || jQuery.cookie ("hide-sb") === "true") {
-		jQuery("#sidebar").hide ();
-	} else {
-		jQuery("#sidebar").show();
-	}
-});
-
-// extend webtrees click function
-jQuery ("#main").on ("click", "#separator", function () {
-	if(responsiveSidebar) {
-		jQuery("#indi_left").toggle();
-	}
-});
-
-// paging tabs on the indi page
-function tabsPaging() {
-	var maxWidth = jQuery("#tabs").width() - jQuery(".tab-prev").width() - jQuery(".tab-next").width() - 20;	
-	var tWidth = 0;
-	
-	jQuery(".tab-visible").each(function() {
-		tWidth = tWidth + jQuery(this).width() + 5;
-		if(tWidth > maxWidth) {
-			jQuery(this).removeClass("tab-visible").addClass("tab-hidden").hide();
-		}		
-	});
-	
-	if(tWidth + jQuery(".tab-hidden:first").width() < maxWidth) {
-		if(jQuery(".tab-first").is(":hidden")) {
-			jQuery(".tab-visible:first").prev(".tab-hidden").removeClass("tab-hidden").addClass("tab-visible").show();
-		} else {
-			jQuery(".tab-visible:last").next(".tab-hidden").removeClass("tab-hidden").addClass("tab-visible").show();
-		}
-	}
-	
-	toggleArrows();
-}
-
-function toggleTabs($tabToShow, $tabToHide){
-	$tabToShow.animate ({width: "show"}, {
-		duration: 300,
-		done: function () {
-			jQuery(this).removeClass("tab-hidden").addClass("tab-visible");
-			if(jQuery(this).hasClass("tab-first")) {
-				jQuery(".tab-prev").hide();
-			}	
-			if(jQuery(this).hasClass("tab-last")) {
-				jQuery(".tab-next").hide();
-			}	
-		}
-	});
-	
-	$tabToHide.animate ({width: "hide"}, {
-		duration: 300,
-		done: function () {
-			jQuery(this).removeClass("tab-visible").addClass("tab-hidden");
-			if(jQuery(this).hasClass("tab-first")) {
-				jQuery(".tab-prev").show();
-			}
-			if(jQuery(this).hasClass("tab-last")) {
-				jQuery(".tab-next").show();
-			}
-		}
-	});
-}
-
-function toggleArrows () {
-	if(jQuery(".tab-first").is(":hidden")) {
-		jQuery(".tab-prev").show();
-	} else {
-		jQuery(".tab-prev").hide();
-	}
-	
-	if(jQuery(".tab-last").is(":hidden")) {
-		jQuery(".tab-next").show();
-	} else {
-		jQuery(".tab-next").hide();
-	}
-}
-
-jQuery("#tabs").on("click", ".tab-next", function() {
-	$tabToShow = jQuery(".tab-visible:last").next(".tab-hidden");
-	$tabToHide = jQuery(".tab-visible:first");
-	toggleTabs($tabToShow, $tabToHide);
-});
-
-jQuery("#tabs").on("click", ".tab-prev", function() {
-	$tabToShow = jQuery(".tab-visible:first").prev(".tab-hidden");
-	$tabToHide = jQuery(".tab-visible:last");
-	toggleTabs($tabToShow, $tabToHide);	
-});
-
-jQuery(".ui-tabs-nav").waitUntilExists(function(){
-	jQuery("li[role=tab]").addClass("tab-visible");
-	jQuery("li[role=tab]").first().addClass("tab-first");
-	jQuery("li[role=tab]").last().addClass("tab-last");
-	
-	jQuery(".tab-first").before('<li class="tab-prev"><i class="icon-larrow"></i></li>');
-	jQuery(".tab-last").after('<li class="tab-next"><i class="icon-rarrow"></i></li>');
-	tabsPaging();
-});
-
-jQuery(window).resize(function() {
-	tabsPaging();
-});
-
-// Styling of the family page
-if(WT_SCRIPT_NAME === "family.php") {
-	// consistent styling (like indi page)
-	jQuery(".facts_table").addClass("ui-widget-content");
-	
-	// add some classes to style particular elements
-	jQuery("#family-table td:first").addClass("left-table").next("td").addClass("right-table");
-	jQuery('.right-table > table tr:eq(1)').addClass("parents-table");
-}
-
-// journal-box correction - remove br's from content. Adjust layout to the news-box layout.
-jQuery(".user_blog_block > br, .journal_box > br").remove();
-jQuery(".journal_box > a[onclick*=editnews]").before('<hr>');
-
-// media list - don't list filenames
-if(jQuery(".media-list").length > 0) {
-	jQuery(".list_item.name2").each(function(){
-		jQuery(this).next("br").remove();
-		jQuery(this).next("a").remove();
-	});
-}
-
 // personboxes
 function jl_expandbox(boxid, bstyle) {
 	var getBox = function () {
@@ -381,21 +220,191 @@ jQuery('[onclick^="expandbox"]').each(function(){
 	});
 });
 
-// census assistant module
-// replace delete button with our own
-jQuery(".census-assistant button").waitUntilExists(function(){
-	jQuery(this).parent("td").html("<i class=\"deleteicon\">");
-});
+/* page specific functions */
 
-jQuery(".deleteicon").waitUntilExists(function() {
-	jQuery(this).on("click", function() {
-		jQuery(this).parents("tr").remove();
+// Sticky footer - correction needed for pedigree page
+if(jQuery("#pedigree-page").length > 0) {
+	jQuery("#content").css("margin-bottom", "50px");
+}
+
+// Move link to change blocks to the footer area.
+if(WT_SCRIPT_NAME === "index.php") {
+	jQuery("#link_change_blocks").appendTo(jQuery("#footer .top"));
+	
+	// journal-box correction - remove br's from content. Adjust layout to the news-box layout.
+	jQuery(".user_blog_block > br, .journal_box > br").remove();
+	jQuery(".journal_box > a[onclick*=editnews]").before('<hr>');
+}
+
+// Styling of the individual page
+if(WT_SCRIPT_NAME === "individual.php") {
+	// Remove personboxNN class from header layout
+	jQuery("#indi_header H3").removeClass("person_boxNN");
+	
+	// relatives tab
+	jQuery("#relatives_content").waitUntilExists(function() {
+		jQuery(this).find(".subheaders").parents("table").css("margin-top", "15px");
 	});
-});	
 
-// use same style for submenu flyout as in the individual sidebar
-jQuery(".census-assistant").waitUntilExists(function() {
-	jQuery(this)
-			.find(".ltrnav").removeClass().addClass("submenu flyout")
-			.find(".name2").removeAttr("style");
-});
+	// When in responsive state hide the indi_left part when sidebar is open.
+	var responsiveSidebar = false;
+
+	// Hide sidebar by default on smaller screens
+	if ($responsive) {
+		jQuery.cookie("hide-sb", true);
+		responsiveSidebar = true;
+	}
+
+	jQuery(window).resize(function() {
+		jQuery("#indi_left").show();
+		if($responsive){
+			responsiveSidebar = true;
+		} else {
+			responsiveSidebar = false;
+		}
+
+		if ($responsive || jQuery.cookie ("hide-sb") === "true") {
+			jQuery("#sidebar").hide ();
+		} else {
+			jQuery("#sidebar").show();
+		}
+	});
+
+	// extend webtrees click function
+	jQuery ("#main").on ("click", "#separator", function () {
+		if(responsiveSidebar) {
+			jQuery("#indi_left").toggle();
+		}
+	});
+
+	// paging tabs on the indi page
+	function tabsPaging() {
+		var maxWidth = jQuery("#tabs").width() - jQuery(".tab-prev").width() - jQuery(".tab-next").width() - 20;	
+		var tWidth = 0;
+
+		jQuery(".tab-visible").each(function() {
+			tWidth = tWidth + jQuery(this).width() + 5;
+			if(tWidth > maxWidth) {
+				jQuery(this).removeClass("tab-visible").addClass("tab-hidden").hide();
+			}		
+		});
+
+		if(tWidth + jQuery(".tab-hidden:first").width() < maxWidth) {
+			if(jQuery(".tab-first").is(":hidden")) {
+				jQuery(".tab-visible:first").prev(".tab-hidden").removeClass("tab-hidden").addClass("tab-visible").show();
+			} else {
+				jQuery(".tab-visible:last").next(".tab-hidden").removeClass("tab-hidden").addClass("tab-visible").show();
+			}
+		}
+
+		toggleArrows();
+	}
+
+	function toggleTabs($tabToShow, $tabToHide){
+		$tabToShow.animate ({width: "show"}, {
+			duration: 300,
+			done: function () {
+				jQuery(this).removeClass("tab-hidden").addClass("tab-visible");
+				if(jQuery(this).hasClass("tab-first")) {
+					jQuery(".tab-prev").hide();
+				}	
+				if(jQuery(this).hasClass("tab-last")) {
+					jQuery(".tab-next").hide();
+				}	
+			}
+		});
+
+		$tabToHide.animate ({width: "hide"}, {
+			duration: 300,
+			done: function () {
+				jQuery(this).removeClass("tab-visible").addClass("tab-hidden");
+				if(jQuery(this).hasClass("tab-first")) {
+					jQuery(".tab-prev").show();
+				}
+				if(jQuery(this).hasClass("tab-last")) {
+					jQuery(".tab-next").show();
+				}
+			}
+		});
+	}
+
+	function toggleArrows () {
+		if(jQuery(".tab-first").is(":hidden")) {
+			jQuery(".tab-prev").show();
+		} else {
+			jQuery(".tab-prev").hide();
+		}
+
+		if(jQuery(".tab-last").is(":hidden")) {
+			jQuery(".tab-next").show();
+		} else {
+			jQuery(".tab-next").hide();
+		}
+	}
+
+	jQuery("#tabs").on("click", ".tab-next", function() {
+		$tabToShow = jQuery(".tab-visible:last").next(".tab-hidden");
+		$tabToHide = jQuery(".tab-visible:first");
+		toggleTabs($tabToShow, $tabToHide);
+	});
+
+	jQuery("#tabs").on("click", ".tab-prev", function() {
+		$tabToShow = jQuery(".tab-visible:first").prev(".tab-hidden");
+		$tabToHide = jQuery(".tab-visible:last");
+		toggleTabs($tabToShow, $tabToHide);	
+	});
+
+	jQuery(".ui-tabs-nav").waitUntilExists(function(){
+		jQuery("li[role=tab]").addClass("tab-visible");
+		jQuery("li[role=tab]").first().addClass("tab-first");
+		jQuery("li[role=tab]").last().addClass("tab-last");
+
+		jQuery(".tab-first").before('<li class="tab-prev"><i class="icon-larrow"></i></li>');
+		jQuery(".tab-last").after('<li class="tab-next"><i class="icon-rarrow"></i></li>');
+		tabsPaging();
+	});
+
+	jQuery(window).resize(function() {
+		tabsPaging();
+	});
+}
+
+// Styling of the family page
+if(WT_SCRIPT_NAME === "family.php") {
+	// consistent styling (like indi page)
+	jQuery(".facts_table").addClass("ui-widget-content");
+	
+	// add some classes to style particular elements
+	jQuery("#family-table td:first").addClass("left-table").next("td").addClass("right-table");
+	jQuery('.right-table > table tr:eq(1)').addClass("parents-table");
+}
+
+
+// media list - don't list filenames
+if(jQuery(".media-list").length > 0) {
+	jQuery(".list_item.name2").each(function(){
+		jQuery(this).next("br").remove();
+		jQuery(this).next("a").remove();
+	});
+}
+
+if(WT_SCRIPT_NAME === "edit_interface.php") {
+	// census assistant module
+	// replace delete button with our own
+	jQuery(".census-assistant button").waitUntilExists(function(){
+		jQuery(this).parent("td").html("<i class=\"deleteicon\">");
+	});
+
+	jQuery(".deleteicon").waitUntilExists(function() {
+		jQuery(this).on("click", function() {
+			jQuery(this).parents("tr").remove();
+		});
+	});	
+
+	// use same style for submenu flyout as in the individual sidebar
+	jQuery(".census-assistant").waitUntilExists(function() {
+		jQuery(this)
+				.find(".ltrnav").removeClass().addClass("submenu flyout")
+				.find(".name2").removeAttr("style");
+	});
+}
