ca600aa8780cc66d3053be67057fc81293e6a5a6
JustCarmen <carmen@justcarmen.nl>
2016-03-04 00:13:09 +0100

    Update bootstrap theme css to version 3.3.6

--- theme.php
+++ theme.php
@@ -260,7 +260,7 @@ class JustLightTheme extends AbstractTheme implements ThemeInterface {
 		// Put a version number in the URL, to prevent browsers from caching old versions.
 		$this->theme_dir = 'themes/justlight/';
 		$this->js_url = 'themes/justlight/js-1.7.4/';
-		$this->bootstrap_url = $this->theme_dir . 'bootstrap-3.3.5/';
+		$this->bootstrap_url = $this->theme_dir . 'bootstrap-3.3.6/';
 		$this->jquery_ui_url = $this->theme_dir . 'jquery-ui-1.11.4/';
 		$this->colorbox_url = $this->theme_dir . 'colorbox-1.5.14/';
 	}
