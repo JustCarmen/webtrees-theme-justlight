408e30dd6758cae07019b64d4ee7f82c6e4f930e
JustCarmen <carmen@justcarmen.nl>
2016-05-30 06:59:12 +0200

    Update folders

--- theme.php
+++ theme.php
@@ -50,7 +50,7 @@ class JustLightTheme extends AbstractTheme implements ThemeInterface {
 
 	/** {@inheritdoc} */
 	public function assetUrl() {
-		return 'themes/justlight/css-1.7.4/';
+		return 'themes/justlight/css-1.7.5/';
 	}
 
 	/** {@inheritdoc} */
@@ -258,7 +258,7 @@ class JustLightTheme extends AbstractTheme implements ThemeInterface {
 	public function hookAfterInit() {
 		// Put a version number in the URL, to prevent browsers from caching old versions.
 		$this->theme_dir	 = 'themes/justlight/';
-		$this->js_url		 = 'themes/justlight/js-1.7.4/';
+		$this->js_url		 = 'themes/justlight/js-1.7.5/';
 		$this->bootstrap_url = $this->theme_dir . 'bootstrap-3.3.6/';
 		$this->jquery_ui_url = $this->theme_dir . 'jquery-ui-1.11.4/';
 		$this->colorbox_url	 = $this->theme_dir . 'colorbox-1.5.14/';
