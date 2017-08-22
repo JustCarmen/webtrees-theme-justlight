1b0a3f4e81b5370cec4d2c5387afda2102f3832a
JustCarmen <carmen@justcarmen.nl>
2017-08-22 23:50:39 +0200

    Styling the individual page

--- js/src/grid.js
+++ js/src/grid.js
@@ -1,12 +1,14 @@
 // Change the default grid on the individual page
-$('.jc-global-individual .wt-main-container > .row > .col-sm-8').each(function() {
-      $(this).addClass('col-12 col-lg-9 col-md-8').removeClass('col-sm-8');
-      $(this).find('.row:first').each(function() {
-        $(this).addClass('d-flex col p-0').removeClass('row');
-        $(this).find('.col-sm-3').addClass('col-2 pl-0').removeClass('col-sm-3');
-        $(this).find('.col-sm-9').addClass('col-10 pr-0').removeClass('col-sm-9');
-      });
-      
-      
+$('.jc-global-individual').each(function() {
+
+  $(this).find('.wt-main-container > .row > .col-sm-8').each(function() {
+        $(this).addClass('col-12 col-lg-9 col-md-8').removeClass('col-sm-8');
+        $(this).find('.row:first').each(function() {
+          $(this).addClass('d-flex col p-0').removeClass('row');
+          $(this).find('.col-sm-3').addClass('col-2 pl-0').removeClass('col-sm-3');
+          $(this).find('.col-sm-9').addClass('col-10 pr-0').removeClass('col-sm-9');
+        });
+  });
+  $(this).find('.wt-main-container > .row > .col-sm-4').addClass('col-12 col-lg-3 col-md-4').removeClass('col-sm-4');
+
 });
-$('.jc-global-individual .wt-main-container > .row > .col-sm-4').addClass('col-12 col-lg-3 col-md-4').removeClass('col-sm-4');
