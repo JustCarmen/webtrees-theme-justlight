3e467a1e5ae3bb9f0a75a66326f8eb23c6ac3b58
JustCarmen <carmen@justcarmen.nl>
2018-01-21 20:05:40 +0100

    Add bootstrap classes to user-page view

--- resources/views/user-page.php
+++ resources/views/user-page.php
@@ -1,4 +1,4 @@
-<h2 class="text-center">
+<h2 class="text-center mb-0 pb-3">
 	<?= $title ?>
 </h2>
 
@@ -10,12 +10,12 @@
 			<?php endforeach ?>
 		</div>
 	<?php else: ?>
-		<div class="col-md-8 wt-main-blocks">
+		<div class="col-md-9 py-3 wt-main-blocks">
 			<?php foreach ($main_blocks as $block_id => $block): ?>
 				<?= view('user-page-block', ['block_id' => $block_id, 'block' => $block, 'tree' => $tree]) ?>
 			<?php endforeach ?>
 		</div>
-		<div class="col-md-4 wt-side-blocks">
+		<div class="col-md-3 py-3 wt-side-blocks">
 			<?php foreach ($side_blocks as $block_id => $block): ?>
 				<?= view('user-page-block', ['block_id' => $block_id, 'block' => $block, 'tree' => $tree]) ?>
 			<?php endforeach ?>
