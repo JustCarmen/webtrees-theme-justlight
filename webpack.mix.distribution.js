e3004cb08485a42dc5e88c9a0d63a3a6d71135dc
JustCarmen <carmen@justcarmen.nl>
2020-08-25 19:36:52 +0200

    Automate distribution package

---
+++ webpack.mix.distribution.js
@@ -0,0 +1,42 @@
+/**
+ * Laravel mix - JustLight theme
+ * 
+ * Output:
+ * 		- dist
+ *        - resources
+ *          - css
+ *          - fonts
+ *          - views
+ *        module.php
+ *        LICENSE.md
+ *        README.md
+ *        
+ */
+
+let mix = require('laravel-mix');
+let config = require('./webpack.mix.config');
+require('laravel-mix-clean');
+
+const dist_dir = 'dist/jc-theme-justlight';
+
+//https://github.com/gregnb/filemanager-webpack-plugin
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+
+mix
+    .setPublicPath(dist_dir)
+    .copyDirectory(config.public_dir, dist_dir + '/resources')
+    .copy('module.php', dist_dir)
+    .copy('LICENSE.md', dist_dir)
+    .copy('README.md', dist_dir)
+    .webpackConfig({
+        plugins: [
+          new FileManagerPlugin({
+            onEnd: {
+                archive: [
+                    { source: './dist', destination: 'dist/justlight-2.0.7.zip'}
+                  ]
+            }
+          })
+        ]
+    })
+    .clean();
