d202a302c7b50dd87c7e843b4540d90d21641c75
JustCarmen <carmen@justcarmen.nl>
2020-08-26 08:28:01 +0200

    Don't zip unwanted files for distribution

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,3 +7,7 @@
 if (process.env.section) {
     require(`${__dirname}/webpack.mix.${process.env.section}.js`);
 }
+
+// Disable mix-manifest.json (https://github.com/JeffreyWay/laravel-mix/issues/580)
+// Prevent the distribution zip file containing an unwanted file
+Mix.manifest.refresh = _ => void 0
