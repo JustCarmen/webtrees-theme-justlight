122dbdc19bc435034c5894da5a7d8750f9b15c0c
JustCarmen <carmen@justcarmen.nl>
2021-03-17 20:57:18 +0100

    Minor release 2.0.13.1

--- webpack.mix.config.js
+++ webpack.mix.config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    version: '2.0.13',
+    version: '2.0.13.1',
     images_dir: 'src/images/',
     webtrees_css_dir: 'vendor/fisharebest/webtrees/resources/css',
     webtrees_npm_dir: 'vendor/fisharebest/webtrees/node_modules',
