4b6b10db4a4da0bffe7135c03de4c387f8414a40
JustCarmen <carmen@justcarmen.nl>
2021-03-18 09:02:59 +0100

    Bump version number

--- module.php
+++ module.php
@@ -79,7 +79,7 @@ return new class extends MinimalTheme implements ModuleThemeInterface, ModuleCus
      */
     public function customModuleVersion(): string
     {
-        return '2.0.13.1';
+        return '2.0.13.2-dev';
     }
 
     /**
