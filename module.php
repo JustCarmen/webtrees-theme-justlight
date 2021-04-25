fd893ee6b6328e03222ffcb2b3e3ff79b41706c3
JustCarmen <carmen@justcarmen.nl>
2021-04-25 11:35:12 +0200

    Bump dev version

--- module.php
+++ module.php
@@ -73,13 +73,13 @@ return new class extends MinimalTheme implements ModuleThemeInterface, ModuleCus
      * We use a system where the version number is equal to the latest version of webtrees
      * Interim versions get an extra sub number
      *
-     * The dev version is always one step above the latest stable version of this module
+     * The dev version is generally one step above the latest stable version of this module
      * The subsequent stable version depends on the version number of the latest stable version of webtrees
      *
      */
     public function customModuleVersion(): string
     {
-        return '2.0.13.2-dev';
+        return '2.0.15-dev';
     }
 
     /**
