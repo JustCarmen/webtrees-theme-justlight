dcea8ba88269c91b8fe9625cc9ea155181c403d8
JustCarmen <carmen@justcarmen.nl>
2022-02-12 08:48:34 +0100

    Fix #99 - Data protection issue with FontAwesome. Replace hosted js with local copies. Remove link to unused css.

--- webpack.mix.theme.js
+++ webpack.mix.theme.js
@@ -33,8 +33,11 @@ const postcss_image_inliner = require("postcss-image-inliner")({
 const postcss_custom_properties = require("postcss-custom-properties")();
 
 if(process.env.NODE_ENV === 'production') {
-    mix.styles(config.public_dir + '/css/justlight.min.css', config.build_dir + '/justlight.min.css')
-    mix.styles(config.public_dir + '/css/justblack.min.css', config.build_dir + '/justblack.min.css')
+    mix
+    .styles(config.public_dir + '/css/justlight.min.css', config.build_dir + '/justlight.min.css')
+    .styles(config.public_dir + '/css/justblack.min.css', config.build_dir + '/justblack.min.css')
+    .copy(config.vendor_build_dir + '/@fortawesome/fontawesome-free/js/regular.min.js', config.public_dir + '/js/fa-regular.min.js')
+    .copy(config.vendor_build_dir + '/@fortawesome/fontawesome-free/js/solid.min.js', config.public_dir + '/js/fa-solid.min.js')
 } else {
     mix
     .setPublicPath('./')
