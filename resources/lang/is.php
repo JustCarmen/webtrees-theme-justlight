8e6ea275c870853da3fc2ca4f54536a30fd4f178
JustCarmen <carmen@justcarmen.nl>
2022-05-01 07:35:10 +0200

    New Islandic translation by Guðjón Sigurðsson

---
+++ resources/lang/is.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Default color palette' => 'Sjálfgefin litatafla',
+    'Allow visitors to switch to another color palette' => 'Leyfa gestum að skipta yfir í aðra litatöflu'
+];
