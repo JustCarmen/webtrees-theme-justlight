660c6c7b06ed6be894dfec28f7e4dfb51dc3b892
JustCarmen <carmen@justcarmen.nl>
2022-05-22 12:05:41 +0200

    2.2.1-release

--- JustlightTheme.php
+++ JustlightTheme.php
@@ -44,7 +44,7 @@ class JustlightTheme extends MinimalTheme implements ModuleThemeInterface, Modul
     /**
      * @var string
      */
-    public const CUSTOM_VERSION = '2.2.0';
+    public const CUSTOM_VERSION = '2.2.1';
 
     /**
      * @var string
