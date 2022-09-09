36a563a4066eebd41a1ab98afaff234042f8d0c5
JustCarmen <carmen@justcarmen.nl>
2022-09-09 16:04:11 +0200

    Replace deprecated laravel mix code

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,3 @@
 if (process.env.section) {
     require(`${__dirname}/webpack.mix.${process.env.section}.js`);
 }
-
-// Disable mix-manifest.json (https://github.com/JeffreyWay/laravel-mix/issues/580)
-// Prevent the distribution zip file containing an unwanted file
-Mix.manifest.refresh = _ => void 0
