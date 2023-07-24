2e21bac49685d022ae18db2effcd860c3aeb16b3
JustCarmen <carmen@justcarmen.nl>
2023-07-24 07:46:57 +0200

    Close #148 - New German translation by Paderman

---
+++ resources/lang/de.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Default color palette' => 'Standard-Farbpalette',
+    'Allow visitors to switch to another color palette' => 'Besuchern den Wechsel zu einer anderen Farbpalette ermöglichen'
+];
