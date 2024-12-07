f223b2a31bee0cf293ed1f24e373b12399354fe6
JustCarmen <carmen@justcarmen.nl>
2024-12-07 17:37:24 +0100

    2.3.0-release

--- webpack.mix.config.js
+++ webpack.mix.config.js
@@ -3,7 +3,7 @@
  */
 
 module.exports = {
-    version: '2.2.9',
+    version: '2.3.0',
     css_dir: 'src/css',
     images_dir: 'src/images',
     webtrees_css_dir: 'vendor/fisharebest/webtrees/resources/css',
