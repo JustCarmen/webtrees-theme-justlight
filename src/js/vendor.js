f8925349af2dce761f8edb0fc7adbc14d766662f
JustCarmen <carmen@justcarmen.nl>
2024-12-15 10:04:14 +0100

    Setup FontAwesome library with just the icons we need

---
+++ src/js/vendor.js
@@ -0,0 +1,20 @@
+// Just import the subset of icons that we use in resources/views/icons/
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+import {
+  // For resources/views/icons/*
+  faCalendarDays
+} from '@fortawesome/free-regular-svg-icons';
+import {
+  // For resources/views/icons/ (faTimes solid version is in webtrees core)*
+  faSquarePlus, faBell, faUser
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  // For resources/views/icons/*
+  faCalendarDays
+);
+library.add(
+  // For resources/views/icons/*
+  faSquarePlus, faBell, faUser
+);
+dom.watch();
