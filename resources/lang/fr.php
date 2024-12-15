a05e7ebddb2d76077c3c2ea15bbd37e2cab91eee
JustCarmen <carmen@justcarmen.nl>
2024-12-15 15:15:31 +0100

    New French translation thanks to SodanWT (#183)

---
+++ resources/lang/fr.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Default color palette' => 'Palette de couleurs par défaut',
+    'Select “JustAuto” to automatically choose the JustLight or JustBlack palette depending on browser color mode.' =>
+    'Sélectionnez “JustAuto” pour choisir automatiquement la palette JustLight ou JustBlack selon le mode de couleur du navigateur.',
+    'Allow visitors to switch to another color palette' => 'Permettre aux visiteurs de changer de palette de couleurs',
+    'Automatically chooses a palette based on browser color mode' => 'Choix de palette automatique selon le mode de couleur du navigateur'
+];
