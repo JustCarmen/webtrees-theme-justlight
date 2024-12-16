226bc86937790bafce8882030f41738a66920974
JustCarmen <carmen@justcarmen.nl>
2024-12-16 11:01:29 +0100

    Add mobile menu icon to the script

--- src/js/vendor.js
+++ src/js/vendor.js
@@ -1,20 +1,21 @@
-// Just import the subset of icons that we use in resources/views/icons/
+/* Just import the subset of icons that we use in:
+- resources/views/icons
+- resources/views/theme/script.js.phtml
+- resources/views/layouts/body/navbar-toggler-secondary.phtml */
 import { dom, library } from '@fortawesome/fontawesome-svg-core';
 import {
-  // For resources/views/icons/*
+  // Regular icons
   faCalendarDays, faUserCircle
 } from '@fortawesome/free-regular-svg-icons';
 import {
-  // For resources/views/icons/ (faTimes solid version is in webtrees core)*
-  faSquarePlus, faBell, faUser
+  // Solid icons (faTimes solid is part of webtrees core)
+  faSquarePlus, faBell, faUser, faEllipsisVertical
 } from '@fortawesome/free-solid-svg-icons';
 
 library.add(
-  // For resources/views/icons/*
   faCalendarDays, faUserCircle
 );
 library.add(
-  // For resources/views/icons/*
-  faSquarePlus, faBell, faUser
+  faSquarePlus, faBell, faUser, faEllipsisVertical
 );
 dom.watch();
